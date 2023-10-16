577ec81666e6b26f20366f9a35388326a6e5dd51
jay <78568609+faunya@users.noreply.github.com>
2023-10-16 15:31:19 -0400

    On 2.6

---
+++ src/Labs/a3/DynamicStyling.js
@@ -0,0 +1,9 @@
+function DynammicStyling() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default DynammicStyling;
--- src/Labs/index.js
+++ src/Labs/index.js
@@ -2,11 +2,11 @@ import Nav from "../Nav";
 import Assignment3 from "./a3";
 
 function Labs() {
- return (
-   <div>
-    <Nav />
-     <Assignment3 />
-   </div>
- );
+  return (
+    <div>
+      <Nav />
+      <Assignment3 />
+    </div>
+  );
 }
 export default Labs;
