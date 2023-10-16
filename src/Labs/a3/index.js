bf6e55f5116ec5efbe08999e3e4e90e14a41cc53
jay <78568609+faunya@users.noreply.github.com>
2023-10-16 15:35:49 -0400

    Finished Lab

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,17 +1,21 @@
-import JavaScript from "./JavaScript";
-import PathParameters from "./PathParameters";
 import Classes from "./Classes";
 import Styles from "./Styles";
+import JavaScript from "./JavaScript";
+import PathParameters from "./PathParameters";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoList from "./todos/TodoList";
 
 function Assignment3() {
- return (
-   <div>
-     <h1>Assignment 3</h1>
-     <Styles/>
-     <Classes/>
-     <PathParameters/>
-     <JavaScript/>
-   </div>
- )
+  return (
+    <div>
+      <h1>Assignment 3</h1>
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
+      <PathParameters />
+      <JavaScript />
+      <TodoList />
+    </div>
+  );
 }
 export default Assignment3;
