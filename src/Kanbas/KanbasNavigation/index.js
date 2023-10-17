40b8db05f6603a0ddc9685b9c4a911825a5b55d2
jay <78568609+faunya@users.noreply.github.com>
2023-10-17 14:45:59 -0400

    navbar color

--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -4,21 +4,23 @@ import "./nav.css";
 
 function KanbasNavigation() {
     const links = ["Account", "Dashboard", "Courses", "Calendar", "Inbox", "History", "Studio", "Commons", "Help"];
-    const icons = ["fa fa-user-circle", "fa-solid fa-gauge", "fa fa-book", "fa fa-calendar", "fa fa-envelope-open", 
-                    "fa fa-clock", "fa fa-tv", "fa fa-arrow-alt-circle-right", "fa fa-question-circle"];
+    const icons = ["fa fa-user-circle", "fa-solid fa-gauge", "fa fa-book", "fa fa-calendar", "fa fa-envelope-open",
+        "fa fa-clock", "fa fa-tv", "fa fa-arrow-alt-circle-right", "fa fa-question-circle"];
     const { pathname } = useLocation();
     return (
+        <div style={{width: 100, height:1000, "backgroundColor": "black"}}>
+            <div className="list-group" style={{ width: 100, "backgroundColor": "black", padding:"5px"}}>
+                {links.map((link, index) => (
+                    <Link
+                        key={index}
+                        to={`/Kanbas/${link}`}
+                        className={`list-group-item ${(pathname.includes(link) && "navbar-item-active") || "navbar-item"}`}>
+                        <FontAwesomeIcon icon={icons[index]} size="2x" color={("Account".match(link) && "grey") || "red"}></FontAwesomeIcon><br />
+                        {link}
+                    </Link>
+                ))}
+            </div>
 
-        <div className="list-group" style={{ width: 100 }}>
-            {links.map((link, index) => (
-                <Link
-                    key={index}
-                    to={`/Kanbas/${link}`}
-                    className={`list-group-item navbar-item ${pathname.includes(link) && "active"}`}>
-                    <FontAwesomeIcon icon={icons[index]} size="2x" color={("Account".match(link) && "white") || "red"}></FontAwesomeIcon><br />
-                    {link}
-                </Link>
-            ))}
         </div>
     );
 }
