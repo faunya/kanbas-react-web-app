1b259e2f00956ce911a4aadf3acc054baf3ba8db
jay <78568609+faunya@users.noreply.github.com>
2023-10-17 16:34:27 -0400

    Dashboard page

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,15 +1,30 @@
 import { Link } from "react-router-dom";
 import db from "../Database";
+import "./dashboard.css";
+
 function Dashboard() {
     const courses = db.courses;
     return (
-        <div>
+        <div style={{ marginLeft: "25px" }}>
             <h1>Dashboard</h1>
-            <div className="list-group">
+            <hr />
+            <h3>Published Courses (3)</h3>
+            <hr />
+            <div className="d-flex flex-row flex-wrap">
                 {courses.map((course) => (
-                    <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item">
-                        {course.name}
-                    </Link>
+                    <div className="card , course-card" style={{ width: "290px" }}>
+                        <img src={require('../Imgs/FYQtAnnaMAE3gdn.jpg')} className="card-img-top" alt="..."></img>
+
+                        <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item" style={{ padding: "10px" }}>
+                            <h5 className="card-title" style={{ fontWeight: "bold" }}>
+                                {course.number} {course.name}
+                            </h5>
+
+                            <p className="card-text" style={{ color: "grey" }}>
+                                {course.number}.{course.startDate}.{course.endDate}
+                            </p>
+                        </Link>
+                    </div>
                 ))}
             </div>
         </div>
