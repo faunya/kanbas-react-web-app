0b6ddc1eabf2bb99f4b4d08bb5c3ad7164fc4a5c
jay <78568609+faunya@users.noreply.github.com>
2023-10-17 13:43:41 -0400

    icon colors

--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useLocation } from "react-router-dom";
+
 function KanbasNavigation() {
     const links = ["Account", "Dashboard", "Courses", "Calendar", "Inbox", "History", "Studio", "Commons", "Help"];
     const icons = ["fa fa-user-circle", "fa-solid fa-gauge", "fa fa-book", "fa fa-calendar", "fa fa-envelope-open", 
@@ -13,7 +14,7 @@ function KanbasNavigation() {
                     key={index}
                     to={`/Kanbas/${link}`}
                     className={`list-group-item ${pathname.includes(link) && "active"}`}>
-                    <FontAwesomeIcon icon={icons[index]} size="2x" color="red"></FontAwesomeIcon><br />
+                    <FontAwesomeIcon icon={icons[index]} size="2x" color={("Account".match(link) && "white") || "red"}></FontAwesomeIcon><br />
                     {link}
                 </Link>
             ))}
