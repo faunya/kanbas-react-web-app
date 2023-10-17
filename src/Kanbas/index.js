cadd994193ced45c05cc0309490608ee9adce7be
jay <78568609+faunya@users.noreply.github.com>
2023-10-17 15:01:02 -0400

    3.2

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,15 +1,20 @@
 import KanbasNavigation from "./KanbasNavigation";
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
+// import "./index.css"; // optionally import CSS files as needed
 function Kanbas() {
   return (
     <div className="d-flex">
       <KanbasNavigation />
       <div>
-        <h1>Account</h1>
-        <h1>Dashboard</h1>
-        <h1>Courses</h1>
+        <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="Account" element={<h1>Account</h1>} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Courses/*" element={<h1>Courses</h1>} />
+        </Routes>
       </div>
     </div>
   );
 }
-export default Kanbas;
+export default Kanbas
