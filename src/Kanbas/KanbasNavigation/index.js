60689ab77bd1c9b375014b22dc303d775f7e8cfb
jay <78568609+faunya@users.noreply.github.com>
2023-10-18 19:03:24 -0400

    renamed navbar css file

--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -1,15 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useLocation } from "react-router-dom";
-import "./nav.css";
+import "./index.css";
 
 function KanbasNavigation() {
     const links = ["Account", "Dashboard", "Courses", "Calendar", "Inbox", "History", "Studio", "Commons", "Help"];
     const icons = ["fa fa-user-circle", "fa-solid fa-gauge", "fa fa-book", "fa fa-calendar", "fa fa-envelope-open",
         "fa fa-clock", "fa fa-tv", "fa fa-arrow-alt-circle-right", "fa fa-question-circle"];
     const { pathname } = useLocation();
+    
     return (
-        <div style={{width: 100, height:1000, "backgroundColor": "black"}}>
-            <div className="list-group" style={{ width: 100, "backgroundColor": "black", padding:"5px"}}>
+        <div style={{ width: 100, height: 1000, "backgroundColor": "black" }}>
+            <div className="list-group" style={{ width: 100, "backgroundColor": "black", padding: "5px" }}>
                 {links.map((link, index) => (
                     <Link
                         key={index}
