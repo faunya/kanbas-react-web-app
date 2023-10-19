847b7131e017f71aa487360dc9f66bfc91a85078
jay <78568609+faunya@users.noreply.github.com>
2023-10-19 16:31:05 -0400

    module buttons

--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -1,27 +1,41 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import db from "../../Database";
+import Button from 'react-bootstrap/Button';
 
 
 function Assignments() {
-  const { courseId } = useParams();
-  const assignments = db.assignments;
-  const courseAssignments = assignments.filter(
-    (assignment) => assignment.course === courseId);
-  return (
-    <div>
-      <h2>Assignments for course {courseId}</h2>
-      <div className="list-group">
-        {courseAssignments.map((assignment) => (
-          <Link
-            key={assignment._id}
-            to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
-            className="list-group-item">
-            {assignment.title}
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+    const { courseId } = useParams();
+    const assignments = db.assignments;
+    const courseAssignments = assignments.filter(
+        (assignment) => assignment.course === courseId);
+    return (
+        <div>
+            <h2>Assignments for course {courseId}</h2>
+
+            <div className="row" >
+                <div>
+                    <Button className="grey-button float-end" variant="light">Primary</Button>
+                    <Button className="grey-button float-end" variant="danger">Primary</Button>
+                    <Button className="grey-button float-end" variant="light">Primary</Button>
+                    <Button className="grey-button float-end" variant="light">Primary</Button>
+                    <Button className="grey-button float-end" variant="light">Primary</Button>
+                </div>
+            </div>
+
+            <hr />
+
+            <div className="list-group">
+                {courseAssignments.map((assignment) => (
+                    <Link
+                        key={assignment._id}
+                        to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+                        className="list-group-item">
+                        {assignment.title}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 }
 export default Assignments;
--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
-import Button from 'react-bootstrap/Button';
+import { Dropdown, Button } from "react-bootstrap";
 
 import "./module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 function ModuleList() {
@@ -12,12 +13,28 @@ function ModuleList() {
     return (
         <div>
             <div className="row" >
-                <div>
-                    <Button className="grey-button float-end" variant="light">Primary</Button>
-                    <Button className="grey-button float-end" variant="danger">Primary</Button>
-                    <Button className="grey-button float-end" variant="light">Primary</Button>
-                    <Button className="grey-button float-end" variant="light">Primary</Button>
-                    <Button className="grey-button float-end" variant="light">Primary</Button>
+                <div className="module-buttons">
+                    <Button className="grey-button float-end mod-button" variant="light">
+                        <FontAwesomeIcon icon="fa fa-ellipsis-v"></FontAwesomeIcon>
+                    </Button>
+                    <Button className="float-end mod-button" variant="danger">
+                        <FontAwesomeIcon icon="fa-solid fa-plus" />
+                        Module
+                    </Button>
+
+                    <Dropdown>
+                        <Dropdown.Toggle className="grey-button float-end mod-button" style={{ backgroundColor: "#e8e8e8" }}>
+                            Publish All
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Button className="grey-button float-end mod-button" variant="light">
+                        <FontAwesomeIcon icon="fa-regular fa-circle-check" style={{ color: "green" , marginRight:"5px"}} />
+                        View Progress
+                    </Button>
+                    <Button className="grey-button float-end mod-button" variant="light">Collapse All</Button>
                 </div>
             </div>
 
