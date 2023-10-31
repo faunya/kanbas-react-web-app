1440197ebf49cfdef849186ff1e86d317071f06e
jay <78568609+faunya@users.noreply.github.com>
2023-10-31 18:57:58 -0400

    Finished 2.2 Lab4

---
+++ src/Labs/a4/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ClickEvent from "./ClickEvent";
+import Add from "./Add";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+
+const Assignment4 = () => {
+  function sayHello() {
+    alert("Hello");
+  }
+
+
+  return (
+    <>
+      <h1>Assignment 4</h1>
+      <Add a={1} b={2} />
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+    </>
+  );
+};
+export default Assignment4
