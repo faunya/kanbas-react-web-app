df8b455dcd0cc445fd6a9257dfd2d1354f1a63c5
jay <78568609+faunya@users.noreply.github.com>
2023-11-02 11:03:52 -0400

    style for dashboard

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -12,22 +12,24 @@ function Dashboard(
             <h1>Dashboard</h1>
             <hr />
 
-            <h5>Course</h5>
-            <input value={course.name} className="form-control"
-                onChange={(e) => setCourse({ ...course, name: e.target.value })} />
-            <input value={course.number} className="form-control"
-                onChange={(e) => setCourse({ ...course, number: e.target.value })} />
-            <input value={course.startDate} className="form-control" type="date"
-                onChange={(e) => setCourse({ ...course, startDate: e.target.value })} />
-            <input value={course.endDate} className="form-control" type="date"
-                onChange={(e) => setCourse({ ...course, endDate: e.target.value })} />
+            <div style={{width:"20%"}}>
+                <h5>Course</h5>
+                <input value={course.name} className="form-control margin-5"
+                    onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+                <input value={course.number} className="form-control margin-5"
+                    onChange={(e) => setCourse({ ...course, number: e.target.value })} />
+                <input value={course.startDate} className="form-control margin-5" type="date"
+                    onChange={(e) => setCourse({ ...course, startDate: e.target.value })} />
+                <input value={course.endDate} className="form-control margin-5" type="date"
+                    onChange={(e) => setCourse({ ...course, endDate: e.target.value })} />
 
-            <button onClick={addNewCourse} >
-                Add
-            </button>
-            <button onClick={updateCourse} >
-                Update
-            </button>
+                <button className="btn btn-success margin-left-5" onClick={addNewCourse} >
+                    Add
+                </button>
+                <button className="btn btn-primary margin-left-5" onClick={updateCourse} >
+                    Update
+                </button>
+            </div>
 
 
             <h3>Published Courses (3)</h3>
@@ -39,6 +41,7 @@ function Dashboard(
 
                         <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item" style={{ padding: "10px" }}>
                             <button
+                                className="btn btn-primary"
                                 onClick={(event) => {
                                     event.preventDefault();
                                     setCourse(course);
@@ -48,6 +51,7 @@ function Dashboard(
 
 
                             <button
+                                className="btn btn-danger margin-left-5"
                                 onClick={(event) => {
                                     event.preventDefault();
                                     deleteCourse(course._id);
