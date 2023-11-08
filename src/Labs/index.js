a80dd95109cfd8fc96127213ad83503cf30293ee
jay <78568609+faunya@users.noreply.github.com>
2023-11-08 13:02:22 -0500

    3.2 finished

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -2,9 +2,10 @@ import store from "./store";
 import { Provider } from "react-redux";
 
 import Nav from "../Nav";
-
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
+import Assignment5 from "./a5";
+
 import { Routes, Route, Navigate }
   from "react-router";
 
@@ -23,6 +24,8 @@ function Labs() {
             element={<Assignment3 />} />
           <Route path="a4"
             element={<Assignment4 />} />
+          <Route path="a5"
+            element={<Assignment5 />} />
         </Routes>
       </div>
     </Provider>
