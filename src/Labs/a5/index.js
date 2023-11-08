40ad173cb4032dca411c42c021aa9cc0ed005ce9
jay <78568609+faunya@users.noreply.github.com>
2023-11-08 14:14:06 -0500

    3.3

--- src/Labs/a5/index.js
+++ src/Labs/a5/index.js
@@ -1,4 +1,5 @@
 import EncodingParametersInURLs from "./EncodingParametersInURLs";
+import WorkingWithArrays from "./WorkingWithArrays";
 import WorkingWithObjects from "./WorkingWithObjects";
 
 function Assignment5() {
@@ -12,6 +13,7 @@ function Assignment5() {
                 </a>
                 <EncodingParametersInURLs />
                 <WorkingWithObjects />
+                <WorkingWithArrays />
             </div>
         </div>
     );
