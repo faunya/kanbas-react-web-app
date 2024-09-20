49670d250abcb46a587f919cf764b10fb1fe0188
loki <78568609+faunya@users.noreply.github.com>
2024-09-20 09:31:37 -0400

    Finish labs

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Labs from "./Labs";
 
 
 function App() {
   return (
-    <div>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
+      </div>
+
+    </HashRouter>
   );
 }
 
