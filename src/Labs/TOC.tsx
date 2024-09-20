9dd62f9ce1a97195363032caa479229c813cbdef
loki <78568609+faunya@users.noreply.github.com>
2024-09-20 11:37:00 -0400

    Added nae

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function TOC() {
   return (
     <ul>
+      <li><a href="https://github.com/faunya/kanbas-react-web-app" id="wd-github">Github repo</a></li>
       <li><Link to="/Labs">Labs</Link></li>
       <li><Link to="/Labs/Lab1">Lab 1</Link></li>
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
