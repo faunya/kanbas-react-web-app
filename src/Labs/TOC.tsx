fb6744f89dd765622ffe08d735a4be2f6eb29e3c
loki <78568609+faunya@users.noreply.github.com>
2024-10-17 16:49:45 -0400

    2.7 lab

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,29 +8,34 @@ export default function TOC() {
           Labs
         </a>
       </li>
+
       <li className="nav-item">
         <a id="wd-a1" href="#/Labs/Lab1"
           className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
           Lab 1
         </a>
       </li>
+
       <li className="nav-item">
         <a id="wd-a2" href="#/Labs/Lab2"
           className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
           Lab 2
         </a>
       </li>
+
       <li className="nav-item">
         <a id="wd-a3" href="#/Labs/Lab3"
           className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
           Lab 3
         </a>
       </li>
+
       <li className="nav-item">
         <a id="wd-k" href="#/Kanbas" className="nav-link">
           Kanbas
         </a>
       </li>
+      
       <li className="nav-item">
         <a id="wd-k" href="https://github.com/faunya" className="nav-link">
           My GitHub
