1862a223e5781b9454e540e99f72efd8b4579622
loki <78568609+faunya@users.noreply.github.com>
2024-10-30 15:30:59 -0400

    3.2

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -11,7 +11,7 @@ import { courses } from "../Database";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
 
-export default function Courses() {
+export default function Courses({ courses }: { courses: any[]; }) {
     const { cid } = useParams();
     const course = courses.find((course) => course._id === cid);
 
