4d8af545b1e074fdea7d054f72588a50a13137b4
loki <78568609+faunya@users.noreply.github.com>
2024-10-30 16:16:01 -0400

    3.3

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,14 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
+import { Provider } from 'react-redux';
+import store from "./Kanbas/store";
 
 
 function App() {
   return (
     <HashRouter>
+      <Provider store={store}>
       <div>
         <Routes>
           <Route path="/" element={<Navigate to="Kanbas" />} />
@@ -18,6 +21,8 @@ function App() {
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
+
+      </Provider>
 
     </HashRouter>
   );
