7feea1b83255137594e7ca49ebe475043782b698
loki <78568609+faunya@users.noreply.github.com>
2024-10-30 17:04:37 -0400

    3.4.4

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -2,17 +2,20 @@ import { Link } from "react-router-dom";
 
 import * as db from "../Database";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function Dashboard(
     { courses, course, setCourse, addNewCourse,
-        deleteCourse, updateCourse}: {
-            courses: any[]; 
-            course: any; 
+        deleteCourse, updateCourse }: {
+            courses: any[];
+            course: any;
             setCourse: (course: any) => void;
-            addNewCourse: () => void; 
+            addNewCourse: () => void;
             deleteCourse: (course: any) => void;
             updateCourse: () => void;
         }) {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const { enrollments } = db;
 
     return (
         <div id="wd-dashboard">
@@ -40,43 +43,49 @@ export default function Dashboard(
             <div id="wd-dashboard-courses" className="row">
                 <div className="row row-cols-1 row-cols-md-5 g-4">
 
-                    {courses.map((course) => (
-                        <div className="wd-dashboard-course col" style={{ width: "300px" }}>
-                            <div className="card rounded-3 overflow-hidden">
-                                <Link to={`/Kanbas/Courses/${course._id}/Home`}
-                                    className="wd-dashboard-course-link text-decoration-none text-dark" >
-                                    <img src={course.image} width="100%" height={160} />
-                                    <div className="card-body">
-                                        <h5 className="wd-dashboard-course-title card-title">
-                                            {course.name} </h5>
-                                        <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
-                                            {course.description} </p>
+                    {courses.filter((course) =>
+                        enrollments.some(
+                            (enrollment) =>
+                                enrollment.user === currentUser._id &&
+                                enrollment.course === course._id
+                        ))
+                        .map((course) => (
+                            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+                                <div className="card rounded-3 overflow-hidden">
+                                    <Link to={`/Kanbas/Courses/${course._id}/Home`}
+                                        className="wd-dashboard-course-link text-decoration-none text-dark" >
+                                        <img src={course.image} width="100%" height={160} />
+                                        <div className="card-body">
+                                            <h5 className="wd-dashboard-course-title card-title">
+                                                {course.name} </h5>
+                                            <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
+                                                {course.description} </p>
 
-                                        <button className="btn btn-primary"> Go </button>
+                                            <button className="btn btn-primary"> Go </button>
 
-                                        <button onClick={(event) => {
-                                            event.preventDefault();
-                                            deleteCourse(course._id);
-                                        }} className="btn btn-danger float-end"
-                                            id="wd-delete-course-click">
-                                            Delete
-                                        </button>
-
-                                        <button id="wd-edit-course-click"
-                                            onClick={(event) => {
+                                            <button onClick={(event) => {
                                                 event.preventDefault();
-                                                setCourse(course);
-                                                console.log();
-                                            }}
-                                            className="btn btn-warning me-2 float-end" >
-                                            Edit
-                                        </button>
+                                                deleteCourse(course._id);
+                                            }} className="btn btn-danger float-end"
+                                                id="wd-delete-course-click">
+                                                Delete
+                                            </button>
 
-                                    </div>
-                                </Link>
+                                            <button id="wd-edit-course-click"
+                                                onClick={(event) => {
+                                                    event.preventDefault();
+                                                    setCourse(course);
+                                                    console.log();
+                                                }}
+                                                className="btn btn-warning me-2 float-end" >
+                                                Edit
+                                            </button>
+
+                                        </div>
+                                    </Link>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
 
                 </div>
             </div>
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -8,6 +8,7 @@ import * as db from "./Database";
 
 import KanbasNavigation from "./Navigation";
 import { useState } from "react";
+import ProtectedRoute from "./Account/ProtectedRoute";
 
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>(db.courses);
@@ -43,14 +44,15 @@ export default function Kanbas() {
           <Routes>
             <Route path="/" element={<Navigate to="Dashboard" />} />
             <Route path="/Account/*" element={<Account />} />
-            <Route path="/Dashboard" element={<Dashboard
-              courses={courses}
-              course={course}
-              setCourse={setCourse}
-              addNewCourse={addNewCourse}
-              deleteCourse={deleteCourse}
-              updateCourse={updateCourse} />} />
-            <Route path="/Courses/:cid/*" element={<Courses courses={courses}/>} />
+            <Route path="/Dashboard" element={<ProtectedRoute>
+              <Dashboard
+                courses={courses}
+                course={course}
+                setCourse={setCourse}
+                addNewCourse={addNewCourse}
+                deleteCourse={deleteCourse}
+                updateCourse={updateCourse} /></ProtectedRoute>} />
+            <Route path="/Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute>} />
             <Route path="/Calendar" element={<h1>Calendar</h1>} />
             <Route path="/Inbox" element={<h1>Inbox</h1>} />
           </Routes>
