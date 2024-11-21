2e205067921ed8dfa04248cd4102f2dbb98c0081
loki <78568609+faunya@users.noreply.github.com>
2024-11-21 16:32:16 -0500

    3.5

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
 import EnvironmentVariables from "./EnvironmentVariables";
+import HttpClient from "./HttpClient";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
@@ -26,6 +28,10 @@ export default function Lab5() {
 
             <WorkingWithObjects />
             <WorkingWithArrays />
+
+            <HttpClient />
+            <WorkingWithObjectsAsynchronously />
+            <WorkingWithArraysAsynchronously />
         </div>
     );
 }
