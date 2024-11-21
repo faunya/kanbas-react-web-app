ddf0cfa72014f8811ea31c385c6463ece58adf3b
loki <78568609+faunya@users.noreply.github.com>
2024-11-21 14:46:58 -0500

    3.3

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,6 +1,7 @@
 import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
 
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
@@ -19,6 +20,8 @@ export default function Lab5() {
             
             <PathParameters />
             <QueryParameters />
+
+            <WorkingWithObjects />
         </div>
     );
 }
