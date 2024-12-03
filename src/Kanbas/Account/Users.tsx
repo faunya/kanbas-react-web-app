c40d6c7e2f228fb6f65511c2d0eb54d8869129c7
loki <78568609+faunya@users.noreply.github.com>
2024-12-03 18:48:40 -0500

    3.6.7

--- src/Kanbas/Account/Users.tsx
+++ src/Kanbas/Account/Users.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import PeopleTable from "../Courses/People/Table";
 import * as client from "./client";
+import { FaPlus } from "react-icons/fa6";
 
 export default function Users() {
     const [users, setUsers] = useState<any[]>([]);
@@ -35,12 +36,31 @@ export default function Users() {
         setUsers(users);
     };
 
+    const createUser = async () => {
+        const user = await client.createUser({
+            firstName: "New",
+            lastName: `User${users.length + 1}`,
+            username: `newuser${Date.now()}`,
+            password: "password123",
+            email: `email${users.length + 1}@neu.edu`,
+            section: "S101",
+            role: "STUDENT",
+        });
+        setUsers([...users, user]);
+    };
+
+
     useEffect(() => {
         fetchUsers();
     }, [uid]);
 
     return (
         <div>
+            <button onClick={createUser} className="float-end btn btn-danger wd-add-people">
+                <FaPlus className="me-2" />
+                Users
+            </button>
+
             <h3>Users</h3>
             <input onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
                 className="form-control float-start w-25 me-2 wd-filter-by-name" />
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -26,12 +26,21 @@ export const findUserById = async (id: string) => {
 };
 
 export const deleteUser = async (userId: string) => {
-    const response = await axios.delete( `${USERS_API}/${userId}` );
+    const response = await axios.delete(`${USERS_API}/${userId}`);
     return response.data;
-  };
+};
 
-  
+export const createUser = async (user: any) => {
+    const response = await axios.post(`${USERS_API}`, user);
+    return response.data;
+};
 
+export const updateUser = async (user: any) => {
+    const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
+    return response.data;
+};
+
+/* logging in-------------------------------------------------------------------------------- */
 export const signin = async (credentials: any) => {
     const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
     return response.data;
@@ -42,11 +51,6 @@ export const signup = async (user: any) => {
     return response.data;
 };
 
-export const updateUser = async (user: any) => {
-    const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
-    return response.data;
-  };
-  
 
 export const profile = async () => {
     const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
